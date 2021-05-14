a957bb1185196ceac159922df24deb65ac7a6ce1
CD Clark III <clifton.clark@gmail.com>
2021-05-14 14:02:09 -0500

    feat: added function for checking dimensions to the Wasm module.

--- wasm/WasmUnitConvert/WasmUnitConvert.cpp
+++ wasm/WasmUnitConvert/WasmUnitConvert.cpp
@@ -19,11 +19,20 @@ std::string UnitConvertString(std::string input, std::string new_unit)
   return std::to_string(GetMagnitudeInUnit(input,new_unit)) + " " + new_unit;
 }
 
+bool HaveSameDimensions(std::string u1, std::string u2)
+{
+  static UnitConvert::UnitRegistry& ureg = UnitConvert::getGlobalUnitRegistry();
+  auto q1 = ureg.makeQuantity<double>(u1);
+  auto q2 = ureg.makeQuantity<double>(u2);
+  return q1.unit().dimension() == q2.unit().dimension();
+}
+
 
 
 EMSCRIPTEN_BINDINGS(WasmUnitConver)
 {
   emscripten::function("UnitConvertString",&UnitConvertString);
   emscripten::function("GetMagnitudeInUnit",&GetMagnitudeInUnit);
+  emscripten::function("HaveSameDimensions",&HaveSameDimensions);
 }
 
